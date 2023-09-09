86a00a7d95d4dec6f1f54c8114b6d12dbf826322
Reed10021 <Reed10021@gmail.com>
2023-09-08 23:16:06 -0500

    Fix CanReplaceMove - should be TRUE, not FALSE.

--- src/pokemon_summary_screen.c
+++ src/pokemon_summary_screen.c
@@ -2240,10 +2240,10 @@ static bool8 CanReplaceMove(void)
 {
     /*if (sMonSummaryScreen->firstMoveIndex == MAX_MON_MOVES
         || sMonSummaryScreen->newMove == MOVE_NONE
-        || IsMoveHm(sMonSummaryScreen->summary.moves[sMonSummaryScreen->firstMoveIndex]) != TRUE)
+        || IsMoveHm(sMonSummaryScreen->summary.moves[sMonSummaryScreen->firstMoveIndex]) != TRUE)*/
         return TRUE;
-    else*/
-        return FALSE;
+    /*else
+        return FALSE;*/
 }
 
 static void ShowCantForgetHMsWindow(u8 taskId)
