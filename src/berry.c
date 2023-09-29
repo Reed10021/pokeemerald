73586831641187a318e713c5e0e45c502c17148a
Reed10021 <Reed10021@gmail.com>
2023-09-29 13:43:43 -0500

    Make berries key off of actual weather instead of route, except for the berry master.

--- src/berry.c
+++ src/berry.c
@@ -15,6 +15,7 @@
 #include "constants/event_object_movement.h"
 #include "constants/items.h"
 #include "constants/region_map_sections.h"
+#include "constants/weather.h"
 
 static u32 GetEnigmaBerryChecksum(struct EnigmaBerry *enigmaBerry);
 static bool32 BerryTreeGrow(struct BerryTree *tree);
@@ -1251,9 +1252,12 @@ static u8 GetBerryCountByBerryTreeId(u8 id)
     struct BerryTree *tree = GetBerryTreeInfo(id);
     const struct Berry *berry = GetBerryInfo(tree->berry);
     u16 currentMap = gMapHeader.regionMapSectionId;
+    u8 currentWeather = gSaveBlock1Ptr->weather;
 
-    if (currentMap == MAPSEC_ROUTE_119 || currentMap == MAPSEC_ROUTE_120 || currentMap == MAPSEC_ROUTE_123)
+    if (currentWeather == WEATHER_RAIN || currentMap == MAPSEC_ROUTE_123) // Berry Master on route 123 tends to the berries.
         return berry->maxYield;
+    else if(currentWeather == WEATHER_RAIN_THUNDERSTORM || currentWeather == WEATHER_DOWNPOUR || currentWeather == WEATHER_ABNORMAL) // Rare weathers offer more berries.
+        return berry->maxYield * 2;
     else
         return gSaveBlock1Ptr->berryTrees[id].berryYield;
 }
