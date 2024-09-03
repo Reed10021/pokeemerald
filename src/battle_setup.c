929c1163e6bbfcde6bdb8e736c714bd2dfe00e2e
Reed10021 <Reed10021@gmail.com>
2024-09-03 15:50:30 -0500

    Actually change rematch chances to 35%.

--- src/battle_setup.c
+++ src/battle_setup.c
@@ -1753,7 +1753,7 @@ static bool32 UpdateRandomTrainerRematches(const struct RematchTrainer *table, u
                 ret = TRUE;
             }
             else if (FlagGet(FLAG_MATCH_CALL_REGISTERED + i)
-             && (Random() % 100) <= 30)  // 31% chance of getting a rematch.
+             && (Random() % 100) <= 34)  // 35% (values 0-34 = 35 values) chance of getting a rematch.
             {
                 SetRematchIdForTrainer(table, i);
                 ret = TRUE;
